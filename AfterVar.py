bbcd739b38f4861ea1c4d088c5e5e16dc2327173
Alfonso0423 <alfonso.lanzuise001@studenti.uniparthenope.it>
2025-12-09 10:47:51 +0100

    creazione header e footer

--- AfterVar.py
+++ AfterVar.py
@@ -7,5 +7,17 @@ app = Flask(__name__)
 def HomePage():
     return render_template('home.html')
 
+@app.route('/regolamento')
 
+def view_regolamento():
+    return render_template('regolamento.html')
 
+@app.route('/infoarbitri')
+
+def view_info_arbitri():
+    return render_template('Info_Arbitri.html')
+
+@app.route('/newscalcio')
+
+def view_news_calcio():
+    return render_template('News_Calcio.html')
