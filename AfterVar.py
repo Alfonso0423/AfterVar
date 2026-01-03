f3b55139a0402aee9d70b7d1d6c62a9b5c457132
emachi05 <emanuele.chiariello001@studenti.uniparthenope.it>
2026-01-03 12:04:42 +0100

    modifiche_db_api_commenti_votazioni

--- AfterVar.py
+++ AfterVar.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, flash
+from flask import Flask, render_template, redirect, url_for, request, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy #for db
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user #preserve user session 
 from werkzeug.security import generate_password_hash, check_password_hash #for pwd security
@@ -19,6 +19,15 @@ login_manager.init_app(app)
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+BAD_WORDS = ["stupido", "scemo", "idiota", "stronzo", "merda", "vaffanculo", "cazzo"] 
+
+def contiene_parolacce(testo):
+    testo_lower = testo.lower()
+    for parola in BAD_WORDS:
+        if parola in testo_lower:
+            return True
+    return False
 
 #user table, usermixin -> function for info of user session, db.model -> to create table
 class User(UserMixin, db.Model):
@@ -54,25 +63,13 @@ class Arbitro(db.Model):
     anno_nascita = db.Column(db.Integer)
     img_url = db.Column(db.String(200), default='static/img/default_ref.png')
 
-
-#video db for less time in page redirect 
-class Video(db.Model):
-    __tablename__ = 'videos'
-
-    id = db.Colummn(db.Integer, primary_key=True)
-    giornata = db.Column(db.Integer, nullable=False)
-    titolo  = db.Column(db.String(100), nullable=False) #squad
-    descrizione = db.Column(db.String(255)) 
-    percorso = db.Column(db.String(100)) #filename video
-    esito_ufficiale = db.Column(db.Boolean, default=True) #user score
-
 #relation user and video
 class Interazione(db.Model):
     __tablename__ = 'interazioni'
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    video_id = db.Column(db.Integer, db.ForeignKey('videos.id'), nullable=False)
+    video_id = db.Column(db.Integer, nullable=False)
     like = db.Column(db.Boolean, default=False)
     dislike = db.Column(db.Boolean, default=False)
     #to save vote of user and save if it is right or not
@@ -82,14 +79,14 @@ class Interazione(db.Model):
 class Commento(db.Model):
     __tablename__ = 'commenti'
 
-    id = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    video_id = db.Column(db.Integer, db.ForeignKey('videos.id'), nullable=False)
+    video_id = db.Column(db.Integer, nullable=False)
     testo = db.Column(db.String(500), nullable=False)
     data = db.Column(db.DateTime, default=datetime.now)
-    stampa_user = db.Column(db.String(50), db.ForeignKey('users.username'), nullable=False)
+    stampa_user = db.Column(db.String(50), nullable=False)
 
-#function to populate the referees table if they don't exist
+#function to populate in default ref and video
 def popola_arbitri():
     if Arbitro.query.first():
         return
@@ -126,6 +123,7 @@ def popola_arbitri():
         db.session.rollback()
         print(f"Errore popolamento: {e}")
 
+
 #Main Menu ON TOP
 
 @app.route('/')
@@ -150,7 +148,8 @@ def Episodi():
 
 @app.route('/classifica')
 def Classifica():
-    return render_template('classifica.html')
+    ranking_user = User.query.order_by(User.punteggio.desc()).limit(10).all() 
+    return render_template('classifica.html', users=ranking_user)
 
 @app.route('/regolamento')
 def Regolamento():
@@ -228,7 +227,98 @@ def Logout():
     logout_user()
     return redirect(url_for('HomePage'))
 
+@app.route('/commenti/<int:video_id>')
+def VisualizzaCommenti(video_id):
+    lista = Commento.query.filter_by(video_id=video_id).order_by(Commento.data.desc()).all()
+    return render_template('commenti.html', commenti=lista)
+
 #MENU IN FOOTER
+
+#API for vote from user
+@app.route('/api/vota_video', methods=['POST'])
+@login_required
+def api_vota_video():
+    data = request.json
+    video_id = data.get('video_id')
+    tipo = data.get('tipo') #is lilke or dislike
+    interazione = Interazione.query.filter_by(user_id=current_user.id, video_id=video_id).first()
+
+    if not interazione:
+        interazione = Interazione(user_id=current_user.id, video_id=video_id)
+        db.session.add(interazione)
+
+    #if like is already clicked, this remove that
+    if tipo == 'like':
+        if interazione.like:
+            interazione.like = False
+        else:
+            interazione.like = True
+            interazione.dislike = False
+    elif tipo == 'dislike':
+        if interazione.dislike:
+            interazione.dislike = False
+        else:
+            interazione.dislike = True
+            interazione.like = False
+    
+    db.session.commit()
+
+    count_like = Interazione.query.filter_by(video_id=video_id, like=True).count()
+    count_dislike = Interazione.query.filter_by(video_id=video_id, dislike=True).count()
+    
+    return jsonify({'success': True, 'likes': count_like, 'dislikes': count_dislike})
+
+@app.route('/api/vota_decisione', methods=['POST'])
+@login_required
+def api_vota_decisione():
+    data = request.json
+    video_id = data.get('video_id')
+    scelta = data.get('scelta') 
+    
+    if scelta == 'giusta':
+        voto_bool = True
+    else:
+        voto_bool = False
+
+    interazione = Interazione.query.filter_by(user_id=current_user.id, video_id=video_id).first()
+    if not interazione:
+        interazione = Interazione(user_id=current_user.id, video_id=video_id)
+        db.session.add(interazione)
+
+    if interazione.giudizio_votazione is None:
+        current_user.punteggio += 10 
+    
+    interazione.giudizio_votazione = voto_bool
+    db.session.commit()
+    
+    return jsonify({'success': True, 'nuovo_punteggio': current_user.punteggio})
+
+@app.route('/api/invia_commento', methods=['POST'])
+@login_required
+def api_invia_commento():
+    data = request.json
+    video_id = data.get('video_id')
+    testo = data.get('testo')
+
+    if contiene_parolacce(testo):
+        return jsonify({'success': False, 'msg': 'Commento rimosso: linguaggio offensivo.'})
+
+    esiste = Commento.query.filter_by(user_id=current_user.id, video_id=video_id).first()
+    if esiste:
+        return jsonify({'success': False, 'msg': 'Hai già commentato questo video.'})
+    
+    nuovo = Commento(
+        user_id=current_user.id, 
+        video_id=video_id, 
+        testo=testo,
+        stampa_user=current_user.username
+    )
+    db.session.add(nuovo)
+    
+    current_user.punteggio += 5
+    db.session.commit()
+    
+    return jsonify({'success': True})
 
 @app.route('/privacy')
 def privacy_policy():
--- static/js/main.js
+++ static/js/main.js
@@ -36,3 +36,71 @@ function ProfileMenu(){
         dropdown.classList.toggle('active_dropdown');
     }
 }
+
+//functions for vote API
+function LikeOrNot(btn, tipo){
+    //for take the correct sec
+    let container = btn.closest('.interactive_sec');
+    let videoId = container.getAttribute('data-id');
+
+    fetch('/api/vota_video', {
+        method:'POST',
+        headers :  { 'Content-Type': 'application/json' },
+        body: JSON.stringify ({ video_id: videoId, tipo:tipo})
+    })
+
+    .then(response => response.json())
+    .then(data => {
+        if(data.success){
+            alert('Likes: ' + data.likes + 'Dislikes: ' + data.dislikes);
+        }
+    });
+}
+
+function RightOrNot(btn, scelta){
+    //for take the correct sec
+    let container = btn.closest('.interactive_sec');
+    let videoId = container.getAttribute('data-id');
+
+    fetch('/api/vota_decisione', {
+        method:'POST',
+        headers :  { 'Content-Type': 'application/json' },
+        body: JSON.stringify ({ video_id: videoId, scelta:scelta})
+    })
+
+    .then(response => response.json())
+    .then(data => {
+        if(data.success){
+            alert('Punteggio aggiornto: ' + data.nuovo_punteggio);
+        }
+    });
+}
+
+function InviaCommento(btn){
+    //for take the correct sec
+    let container = btn.closest('.interactive_sec');
+    let videoId = container.getAttribute('data-id');
+    let input = container.querySelector('.comment_box');
+    let testo = input.value;
+
+    //if is empty
+    if(!testo)
+        return
+
+    fetch('/api/invia_commento', {
+        method:'POST',
+        headers :  { 'Content-Type': 'application/json' },
+        body: JSON.stringify ({ video_id: videoId, testo:testo})
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data.success){
+            alert('Commento Inviato Correttamente');
+            input.value='';
+            input.disabled=true;
+            btn.disabled=true;
+        }else{
+            alert (data.msg);
+        }
+    });
+}
